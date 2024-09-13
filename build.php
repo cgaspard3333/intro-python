b4ab632571ba66f52bc4d5e9d319f6abde0dd4ca
Grégoire Passault <g.passault@gmail.com>
2024-09-13 10:25:07 +0200

    Skeleton

---
+++ build.php
@@ -0,0 +1,39 @@
+<?php
+
+include('vendor/autoload.php');
+
+/**
+ * Include here your custom libraries
+ */
+
+$slidey = new Gregwar\Slidey\Slidey;
+
+/**
+ * Customizing template
+ */
+
+$slidey->addCss('css/style.css');
+
+// Sets the title prefix
+$slidey->setTitle('Skeleton');
+
+/**
+ * Adding custom directories
+ */
+
+// This will copy the directory "css" to the target directory
+// $slidey->copy('css', 'css');
+// $slidey->copy('img', 'img');
+
+// Adding zip files
+$slidey
+    ->copy('favicon.ico')
+    // ->mkdir('files')
+    // ->copy(__DIR__.'/files/*.zip', 'files/')
+;
+
+// Runs the build to the web directory
+$target = 'web/';
+
+
+$slidey->build($target);
---
+++ pages/index.rst
@@ -0,0 +1,11 @@
+Slidey skeleton
+===============
+
+.. slide::
+
+Enseignants et support
+----------------------
+
+.. important::
+
+    Hello!
